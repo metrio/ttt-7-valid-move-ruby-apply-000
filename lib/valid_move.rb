4a90e6d654a3f709a5697382748bc255779b73f1
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-20 14:49:30 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -9,7 +9,7 @@ def position_taken?(array, ind)
 
   if (array[ind] == " "|| array[ind] == "" || array[ind] == nil)
    return false
-  else (array[ind] == "X" || array[ind] == "O")
+  else 
    return true
   end
 end
