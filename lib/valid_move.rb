e0d606b4ee1777583e090ddad71d440a8366465e
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-20 14:49:49 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -14,8 +14,8 @@ def position_taken?(array, ind)
   end
 end
 
-def on_board? (num)
-  if num.between? (0, 8) == true
+def on_board?(num)
+  if num.between?(0, 8) == true
    return true
   else
    return false
