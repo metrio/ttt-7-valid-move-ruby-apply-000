2e6563fddfffa25adabf95582eef34a00f869c94
Demetrio Lima <dlima.nyit@gmail.com>
2020-08-20 14:54:08 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -7,23 +7,23 @@ def valid_move?(board, index)
 def position_taken?(board, index)
 
   if (board[index] == " "|| board[index] == "" || board[index] == nil)
-   return false
-  else 
-   return true
+   false
+  elsif 
+   true
   end
 end
 
 def on_board?(num)
   if num.between?(0, 8) == true
    true
-  else
+  elsif
    false
   end
 end
     
 if ((position_taken?(board, index)) == false && (on_board?(index) == true))
   true
-else
+elsif
   false
 end
 
